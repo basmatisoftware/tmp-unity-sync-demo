937833a56164deb33b19d380d606a3b9732ebc11
NA <NA@none.com>
2020-11-25 14:23:57 -0800

    Added a comment.

--- Assets/WidnowsUser/WinScript.cs
+++ Assets/WidnowsUser/WinScript.cs
@@ -7,7 +7,7 @@ public class WinScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Made some code changes.
     }
 
     // Update is called once per frame
