b006610b97e1e54013e965ca3dbab24fc4e34d79
Jim on Linux <you@example.com>
2020-11-26 16:27:49 -0800

    Added a script that make foo bar ar aooom foomlaksj aaldskfj .

---
+++ Assets/LinuxUser/FooBarrOoommOool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FooBarrOoommOool : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Whappppowojjr if fuao aowkejr aof ososdifakljf s 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
